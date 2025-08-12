5e700f3143aa98ea44942b020b30c32fb575bb63
Your Name <you@example.com>
2025-08-13 03:17:29 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import express from 'express';
-import cors from 'cors';
-import verificationRoutes from './routes/verificationRoutes.js';
-
-const app = express();
-
-app.use(cors());
-=======
 // server.js
 import dotenv from 'dotenv';
 import express from 'express';
@@ -20,7 +11,6 @@ const app = express();
 
 // Middleware
 app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
->>>>>>> de9464cdbd3f1c9eab3d077282a3ed9d97051597
 app.use(express.json());
 
 app.use('/', verificationRoutes);
@@ -29,15 +19,11 @@ app.get('/', (req, res) => {
   res.send('Backend is running...');
 });
 
-<<<<<<< HEAD
-const PORT = 5000;
-=======
 // Routes
 app.use('/api/auth', authRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
->>>>>>> de9464cdbd3f1c9eab3d077282a3ed9d97051597
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
