e587e1160f50105f3882463d65d2113b47196ca1
Abacco-dev <anusha.abacco@gmail.com>
2025-08-13 02:37:57 +0530

    verification

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import verificationRoutes from './routes/verificationRoutes.js';
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test route
+app.use('/', verificationRoutes);
+
 app.get('/', (req, res) => {
   res.send('Backend is running...');
 });
 
-// Start server
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
