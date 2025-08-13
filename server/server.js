3eb918bb048e552f9e1e79734cb2da728c6e41f8
Abacco-dev <anusha.abacco@gmail.com>
2025-08-13 17:45:07 +0530

    dashboardlayout,packagejson

--- server/server.js
+++ server/server.js
@@ -1,18 +1,23 @@
+// server.js
 import express from 'express';
 import cors from 'cors';
-import verificationRoutes from './routes/verificationRoutes.js';
+import verificationRoutes from './routes/verificationRoutes.js'; // Use .js extension in ES modules
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Use routes
 app.use('/', verificationRoutes);
 
+// Test route
 app.get('/', (req, res) => {
   res.send('Backend is running...');
 });
 
+// Start server
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
