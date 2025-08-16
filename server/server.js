ff665de89b79984998c14d8cf3f7f90149229222
Abacco-dev <anusha.abacco@gmail.com>
2025-08-16 19:32:57 +0530

    verfication

--- server/server.js
+++ server/server.js
@@ -1,34 +1,21 @@
-// server.js
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes.js';
-import verificationRoutes from './routes/verificationRoutes.js';
-
-
-// Load environment variables
-dotenv.config();
+// server/server.js
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import verificationRoutes from "./routes/verificationRoutes.js";
 
 const app = express();
-
-// Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
-app.use(cors());
-app.use(express.json());
-
-// Use routes
-app.use('/verification', verificationRoutes);
-app.use('/api/auth', authRoutes);
-
-// Test route
-app.get('/', (req, res) => {
-  res.send('Backend is running...');
-});
-
-
-// Start server
 const PORT = process.env.PORT || 5000;
 
+/* Middlewares */
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Routes */
+app.use("/verification", verificationRoutes);
+
+/* Start */
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
