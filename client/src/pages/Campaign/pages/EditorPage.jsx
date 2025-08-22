02e2bda863c0cedc42439eb3acfe79b22342050e
Rasul <rasulabaccotech@gmail.com>
2025-08-22 23:40:17 +0530

    update

--- client/src/pages/Campaign/pages/EditorPage.jsx
+++ client/src/pages/Campaign/pages/EditorPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import CanvasArea from '../components/Editor/CanvasArea';
-import Toolbox from '../components/Editor/Toolbox';
-import PropertiesPanel from '../components/Editor/PropertiesPanel';
-import Layout  from '../Components/Layout/Layout';
+import CanvasArea from '../Components/Editor/CanvasArea';
+import Toolbox from '../Components/Editor/Toolbox';
+import PropertiesPanel from '../Components/Editor/PropertiesPanel';
+import Layout from '../Components/Layout/Layout';
 
 const EditorPage = () => {
     return (
