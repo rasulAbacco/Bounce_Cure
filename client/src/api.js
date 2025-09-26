217348fa3266615ca4dad729a4a80d06bdfdf5cc
AshokAbacco <ashokgowd34@gmail.com>
2025-09-26 20:33:37 +0530

    update

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,19 @@
-// frontend/src/api.js
+// // frontend/src/api.js
+// import axios from "axios";
+// export const api = axios.create({ baseURL: "http://localhost:5000/api" });
+
 import axios from "axios";
-export const api = axios.create({ baseURL: "http://localhost:5000/api" });
+
+export const api = axios.create({
+  baseURL: "http://localhost:5000/api",
+});
+
+// Add token to each request
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
